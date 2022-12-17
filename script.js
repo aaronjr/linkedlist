8a5746be4a7532d9ff1177c8d5bb89683b29b7f4
Aaron Richards <aaron_richards@hotmail.co.uk>
2022-12-17 19:10:22 +0000

    continued to add functionality inline with task

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ class Node{
 }
 
 class List{
-    head(node){
-        this.headNode = node
-    }
+    // get all data points from list
     all(){
         let current = this.headNode
         let next = current.next
@@ -21,7 +19,11 @@ class List{
         }
         return allData
     }
-    toEnd(node){
+    // add to end of list
+    append(node){
+        if(!this.headNode){
+            this.headNode = node
+        } else {
         let current = this.headNode
         let next = current.next
         while(next != null){
@@ -29,8 +31,25 @@ class List{
             next = next.next
         }
         current.next = node
+        }
     }
-    last(){
+    prepend(node){
+        node.next = this.headNode
+        this.headNode = node
+    }
+    size(){
+        let counter = 1;
+        let current = this.headNode
+        let next = current.next
+        while(next != null){
+            current = next
+            next = next.next
+            counter += 1
+        }
+        return counter
+    }
+    // get back last item
+    tail(){
         let current = this.headNode
         let next = current.next
         while(next != null){
@@ -39,7 +58,65 @@ class List{
         } 
         return current.data
     }
-
+    at(index){
+        let counter = 1
+        let current = this.headNode
+        let next = current.next
+        while(counter !== index){
+            current = next
+            next = next.next
+            counter ++
+        }
+        return current.data
+    }
+    pop(){
+        let current = this.headNode
+        let next = current.next
+        while(next.next !== null){
+            current = next
+            next = next.next
+        }
+        current.next = null
+    }
+    contains(target){
+        let current = this.headNode
+        let next = current.next
+        while(next.next !== null){
+            if(current.data == target) {
+                return true
+            }
+            current = next
+            next = next.next
+        }
+        return false
+    }
+    find(target){
+        let current = this.headNode
+        let next = current.next
+        let counter = 0
+        while(current.next !== null){
+            if(current.data == target) {
+                return counter
+            } else if (next.data == target){
+                return counter
+            }
+            counter += 1
+            current = next
+            next = next.next
+        }
+        return null
+    }
+    toString(){
+        let current = this.headNode
+        let next = current.next
+        let string = `(${current.data}) -> `
+        while(current.next !== null){
+            string += `(${next.data}) -> `
+            current = next
+            next = next.next
+        }
+        return string += `(null)`
+    }
 }
 
 const one = new Node(1)
@@ -47,21 +124,67 @@ const two = new Node(2)
 const three = new Node(3)
 const four = new Node(4)
 const five = new Node(5)
+const six = new Node(6)
 
 // console.log(newNode.data, newNode.next)
 const theList = new List()
 
 // set head of list
-theList.head(one)
+theList.append(one)
+theList.append(two)
+theList.append(three)
+theList.append(four)
+theList.append(five)
 
-// add nodes to end
-theList.toEnd(two)
-theList.toEnd(three)
-theList.toEnd(four)
-theList.toEnd(five)
+// check for head
+console.log('First head:', theList.headNode.data)
+
+// change head node
+theList.prepend(six)
+// check for head if changed
+console.log('Changed head:', theList.headNode.data)
 
 // check for all data points
-console.log(theList.all())
+console.log('All items:', theList.all())
 
 // check last data point
-console.log(theList.last())
+console.log('Last item:', theList.tail())
+
+// length of list
+console.log('Length:', theList.size())
+
+// at 3rd index
+// counting from 1 not 0
+// 3rd item in list
+console.log('At index:', theList.at(3))
+
+// remove last item
+theList.pop()
+
+// check new list
+console.log('List after pop:', theList.all())
+
+// find an item
+// checks for 5 which was previously removed
+console.log('Contains 5:', theList.contains(5))
+
+// checks for 6 which is present
+console.log('Contains 6:', theList.contains(6))
+
+// check list and return index of the matched target
+// returns the index
+console.log('Find 4', theList.find(4))
+
+// return list as a string
+console.log(theList.toString())
+
+// insert new node
+const seven = new Node(7)
+// function
+console.log('All inc 7:' )
+
+
+// insert new node
+const eight = new Node(8)
+// function
+console.log('All inc 8:' )
